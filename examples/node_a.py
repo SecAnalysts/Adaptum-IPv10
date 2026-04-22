7f554c9872cf52bf96de2839911dada69b521d03
budyz <30666229+SecAnalysts@users.noreply.github.com>
2026-04-22 13:45:53 +0700

    Create node_a.py

---
+++ examples/node_a.py
@@ -0,0 +1,14 @@
+from src.node import IPv10Node
+import time
+
+A = IPv10Node("127.0.0.1", 5001)
+A.peers = [("127.0.0.1", 5002)]
+A.start()
+
+time.sleep(1)
+A.connect_peer("127.0.0.1", 5002)
+
+time.sleep(2)
+A.send_message("TARGET_NODE_C_ADDRESS", "Hello from A")
+
+while True: time.sleep(1)
